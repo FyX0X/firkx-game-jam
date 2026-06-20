0b698b58d5c80971dfbc8afe4a55040cc2e35026
Martin Ergo <martinergo06@gmail.com>
2026-06-20 21:31:54 +0200

    added debug fly

--- Player/player.gd
+++ Player/player.gd
@@ -14,12 +14,22 @@ var _current_target: Node = null
 @export var mouse_sensitivity: float = 0.003
 @export var tilt_limit = deg_to_rad(75)
 
+var fly_debug: bool = false
 
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta: float) -> void:
+	if fly_debug:
+		_process_debug_flying(delta)
+	else:
+		_process_movement(delta)
+	move_and_slide()
+	
+	raycast_check()
+
+func _process_movement(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -39,10 +49,7 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.z = move_toward(velocity.z, 0, speed)
 
-	move_and_slide()
-	
-	raycast_check()
-	
+
 func raycast_check() -> void:
 	if raycast.is_colliding():
 		var interactable = raycast.get_collider()
@@ -70,3 +77,17 @@ func _unhandled_input(event: InputEvent) -> void:
 
 func get_inventory() -> Inventory:
 	return inventory
+
+
+func _process_debug_flying(delta: float) -> void:
+	var input_dir := Input.get_vector("left", "right", "forward", "backward")
+	var direction := (camera_arm.global_basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	var up_down := 0.0
+	if Input.is_action_pressed("jump"):
+		up_down = 1.0
+	if Input.is_action_pressed("crouch"):  # add this action too
+		up_down = -1.0
+	
+	var target_velocity: Vector3 = direction * speed + Vector3.UP * up_down * speed
+	velocity = velocity.lerp(target_velocity, delta * 10.0)  # smooth it out
--- UI/debug_panel.gd
+++ UI/debug_panel.gd
@@ -8,15 +8,18 @@ extends Control
 @onready var speed_slider: HSlider = $Categories/Movement/SpeedSlider
 @onready var speed_label: Label = $Categories/Movement/SpeedLabel
 
+var player: Player
 var player_inventory: Inventory
 
 const ITEMS = ["iron_ore", "coal", "copper_ore", "stone", "wood", "iron_ingot", "gear"]
 
 func _ready():
-	player_inventory = get_tree().get_first_node_in_group("player").get_node("Inventory")
+	player = get_tree().get_first_node_in_group("player")
+	player_inventory = player.get_node("Inventory")
 	for item in ITEMS:
 		item_option.add_item(item)
 	amount_slider.value_changed.connect(func(v): amount_label.text = str(int(v)))
+	speed_slider.value = player.speed
 
 func _input(event):
 	if event.is_action_pressed("debug"):   # bind F3 in InputMap
@@ -34,9 +37,9 @@ func _on_remove_button_pressed() -> void:
 	player_inventory.remove_item(item, amt)
 
 func _on_fly_button_toggled(toggled_on: bool) -> void:
-	print("TODO: on_fly_button() Implement this.")
+	player.fly_debug = toggled_on
 
 
 func _on_speed_slider_value_changed(value: float) -> void:
 	speed_label.text = "speed: " + str(value)
-	print("TODO: speed slider")
+	player.speed = value
