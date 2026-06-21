066d7dd12e226b42eeedb4c9153c909b9b3d81d2
Martin Ergo <martinergo06@gmail.com>
2026-06-21 15:16:34 +0200

    working zones detection and damage

--- World/DamageZone/damage_zone.gd
+++ World/DamageZone/damage_zone.gd
@@ -3,6 +3,7 @@ extends Area3D
 
 @export var damage_per_second: float = 20.0
 @export var zone_type: String = "desert"  # just a label for UI
+@export var grace_period: float = 5
 
 
 # Called when the node enters the scene tree for the first time.
@@ -15,8 +16,12 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_body_entered(body: Node3D) -> void:
+	print(str(self))
+	print("entered by : " + str(body))
 	if body is Player:
 		body.enter_zone(self)
+	else: # should never happen since checks on layer 2 = Player
+		print(self, "Unexepected Body entry was not player")
 
 func _on_body_exited(body: Node3D) -> void:
 	if body is Player:
