204191933b71326106ca5ceaba8679da6c02d4fc
Hakibara_tour <gumgumedgar@gmail.com>
2026-06-21 20:06:21 +0200

    Fixed the changes of the merge

--- Buildings/Science/science_table.gd
+++ Buildings/Science/science_table.gd
@@ -18,7 +18,7 @@ const science_needed : Dictionary = {
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	super._ready()
-  process_speed = 10
+	process_speed = 10
 
 
 func _process(delta: float) -> void:
