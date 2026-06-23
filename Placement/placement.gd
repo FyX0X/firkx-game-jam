7a6f4ce8db70efae2d005ae180315edd402dcf33
Martin Ergo <martinergo06@gmail.com>
2026-06-23 10:58:21 +0200

    fix is placable return type

--- Placement/placement.gd
+++ Placement/placement.gd
@@ -40,9 +40,9 @@ func _process(_delta: float) -> void:
 	_update_hologram()
 
 
-func is_placeable(hologram : Building):
+func is_placeable(hologram : Building) -> bool:
 	if hologram == null:
-		return
+		return false
 	var location : bool = true
 	if  (hologram is Drill and not ray.get_collider() is BigOre):
 		location = false
