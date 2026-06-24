39c42cb2c305acfadd09eb7b059362056a7f6716
Martin Ergo <martinergo06@gmail.com>
2026-06-24 19:01:16 +0200

    add production/demand var in power grid

--- PowerGrid/power_grid.gd
+++ PowerGrid/power_grid.gd
@@ -4,6 +4,9 @@ extends RefCounted
 var grid_id : int 
 var energy : int = 0
 
+var demand: int = 0
+var production: int = 0
+
 var connected_buildings : Array[Node3D] = []
 
 func _init(id : int):
@@ -17,8 +20,14 @@ func add_building(building : Node3D):
 
 func _recalculate_power():
 	energy = 0
+	production = 0
+	demand = 0
 	for building in connected_buildings:
 		energy += building.energy
+		production += maxi(building.energy, 0)
+		demand += maxi(-building.energy, 0)
+	print("energy: %d, demand: %d, production: %d" % [energy, demand, production])
+	assert(energy == (production - demand))
 	var powered = energy >= 0
 	for building in connected_buildings:
 		building.set_powered(powered)
