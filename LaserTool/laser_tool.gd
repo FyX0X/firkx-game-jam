0cb95a5a4050290aa5995216897936c6b4db7c3a
Hakibara_tour <gumgumedgar@gmail.com>
2026-06-25 23:56:09 +0200

    Laser working ruban

--- LaserTool/laser_tool.gd
+++ LaserTool/laser_tool.gd
@@ -14,13 +14,68 @@ var _connected_target: Node  = null
 var qte_bonus_time : float = 0.0
 
 var _qte_active : bool = false
-var bonus_duration : float = 0.3
+var bonus_duration : float = 0.25
+
+var laser_material : Material = preload("res://assets/Material/purple_laser.tres")
+var subdivisions : int = 50
+var thickness : float = 0.3
+
+var im_mesh : ImmediateMesh 
+
+func _ready() -> void:
+	im_mesh = ImmediateMesh.new()
+	beam.mesh = im_mesh
+	beam.material_override = laser_material
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("attack"):
 		_do_laser(delta)
 	else:
 		_stop_laser()
+
+func create_curved_laser(delta :float):
+	var start : Vector3 = player.find_child("Laser",true,false).global_position
+	var end : Vector3 = player.raycast.get_collision_point()
+	if not player.raycast.is_colliding():
+		return
+	start = beam.to_local(start)
+	end = beam.to_local(end)
+	im_mesh.clear_surfaces()	
+	im_mesh.surface_begin(Mesh.PRIMITIVE_TRIANGLE_STRIP)
+	var time_scale = Time.get_ticks_msec() * 0.01
+	var direction = end - start
+	var total_distance = direction.length()
+	
+	var camera = get_viewport().get_camera_3d()
+	var previous_point = start
+	if not camera: return
+	var cam_pos = beam.to_local(camera.global_position)
+	
+	for i in range(1, subdivisions + 1):
+		var t = float(i) / subdivisions
+		
+		var base_point = start.lerp(end, t)
+	
+		var offset_y = sin((t * total_distance * 5.0)- time_scale) * 0.2
+		var offset_x = sin((t * total_distance * 5.0)-time_scale) * 0.2 
+		
+		var current_point = base_point + Vector3(offset_x, offset_y, 0.0)
+		
+		var to_cam = (cam_pos - current_point).normalized()
+		
+		# Produit vectoriel (Cross) pour trouver l'axe gauche/droite perpendiculaire à la caméra
+		var width_axis = direction.cross(to_cam).normalized() * (thickness / 2.0)
+		
+		# Sécurité si on regarde le laser de face parfaitement (évite un crash de rendu)
+		if width_axis.is_zero_approx():
+			width_axis = Vector3.UP * (thickness / 2.0)
+		
+		# On ajoute 2 sommets (un à gauche, un à droite) pour former le ruban
+		im_mesh.surface_add_vertex(current_point - width_axis)
+		im_mesh.surface_add_vertex(current_point + width_axis)
+		
+	im_mesh.surface_end()
+	
 
 func _do_laser(delta: float) -> void:
 	var damage = 0
@@ -41,6 +96,7 @@ func _do_laser(delta: float) -> void:
 			target.resource_yielded.connect(_on_resource_yielded)
 			
 	beam.visible = true
+	create_curved_laser(delta)
 	if player._current_state == Player.State.NORMAL:
 		player.set_state(Player.State.ATTACKING)
 	
