2b8365157f74c7b7cf95b2fcbafb55020677af63
Martin Ergo <martinergo06@gmail.com>
2026-06-26 00:32:36 +0200

    fix end scene

--- EndScene/end_scene.gd
+++ EndScene/end_scene.gd
@@ -3,6 +3,8 @@ extends Control
 
 @onready var end_sound_player: AudioStreamPlayer = $EndSound
 @onready var cinematic_player: VideoStreamPlayer = $CinematicPlayer
+@onready var title_label: Label = $Label
+@onready var quit_button: Button = $VBoxContainer/QuitButton
 
 @export var title: String = "You Win !"
 @export var quit_button_text: String = "Quit"
@@ -13,21 +15,23 @@ extends Control
 func _ready() -> void:
 	assert(cinematic != null)
 	assert(sound != null)
+	title_label.text = title
+	quit_button.text = quit_button_text
 	cinematic_player.stream = cinematic
 	end_sound_player.stream = sound
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	cinematic_player.show()
-	
-
-
-func _on_failed_video_finished() -> void:
-	cinematic_player.hide()
-	end_sound_player.play()
+	cinematic_player.play()
 
 
 func _on_main_menu_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://UI/MainMenu/main_menu.tscn")
 
 
-func _on_rage_quit_button_pressed() -> void:
+func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_cinematic_player_finished() -> void:
+	cinematic_player.hide()
+	end_sound_player.play()
