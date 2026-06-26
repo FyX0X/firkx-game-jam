5372a9e5400dd6f93fc56748d15c919c3e8543c5
Martin Ergo <martinergo06@gmail.com>
2026-06-27 00:21:54 +0200

    change default music volume

--- Settings/game_settings.gd
+++ Settings/game_settings.gd
@@ -19,7 +19,7 @@ const hologram_msg: Dictionary = {
 
 var difficulty: Difficulty = Difficulty.NORMAL
 var _master_volume: float = 1
-var _music_volume: float = 1
+var _music_volume: float = 0.5
 var _sfx_volume: float = 1
 
 func get_master_volume() -> float:
