750650e5d005df4abf27d786fef29be47829420e
Martin Ergo <martinergo06@gmail.com>
2026-06-27 07:07:35 +0200

    add crosshair accessibility mode

--- UI/SettingsMenu/settings_menu.gd
+++ UI/SettingsMenu/settings_menu.gd
@@ -1,9 +1,9 @@
 extends Control
 
-@onready var master_volume_slider: HSlider = $VBoxContainer/MasterSlider
-@onready var music_volume_slider: HSlider = $VBoxContainer/MusicSlider
-@onready var sfx_volume_slider: HSlider = $VBoxContainer/SFXSlider
-
+@onready var master_volume_slider: HSlider = $HBoxContainer/MarginContainer/Sound/MasterSlider
+@onready var music_volume_slider: HSlider = $HBoxContainer/MarginContainer/Sound/MusicSlider
+@onready var sfx_volume_slider: HSlider = $HBoxContainer/MarginContainer/Sound/SFXSlider
+@onready var crosshair_button: CheckButton = $HBoxContainer/Accessibility/CrosshairCheckButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,6 +11,8 @@ func _ready() -> void:
 	master_volume_slider.value = GameSettings.get_master_volume()
 	music_volume_slider.value = GameSettings.get_music_volume()
 	sfx_volume_slider.value = GameSettings.get_sfx_volume()
+	crosshair_button.button_pressed = GameSettings.crosshair_enabled
+	
 
 
 func _on_exit_button_pressed() -> void:
@@ -27,3 +29,7 @@ func _on_music_slider_value_changed(value: float) -> void:
 
 func _on_sfx_slider_value_changed(value: float) -> void:
 	GameSettings.set_sfx_volume(value)
+
+
+func _on_crosshair_check_button_toggled(toggled_on: bool) -> void:
+	GameSettings.set_crosshair(toggled_on)
